50b96f57a8c2f3252800e2381e8067ee5cdceca6
Enrique Gambra <gambra7@gmail.com>
2019-10-19 11:26:27 -0400

    Deleted the original get_adelphi_info.py script and renamed the new more efficient JSON script to get_adelphi_info.py

--- Adelphi Academic Calendar/get_adelphi_info.py
+++ Adelphi Academic Calendar/get_adelphi_info.py
@@ -35,57 +35,97 @@ class AdelphiInfo:
     def create_json_file(self, data):
         """Creates the JSON file"""
         # Cleans up the code even more, getting rid of whitespace, double // and other strings
-        more_parsed = list()
-        for element in data:
-            data_parsed = element.strip()
-            more_refined = data_parsed.replace("\\", "")
-            even_more_refined = more_refined.replace("n'b'", "")
-            clean = even_more_refined.strip()
-            more_parsed.append(clean)       # Appended to an even more refined list
+        more_parsed = self._clean_up_text(data)
 
-        # Create two lists, one for dates and the other for events
         dates = list()
         events = list()
 
         # Go through the more_parsed list, find elements that start with a month and append it to the dates list
         # And then the next index in the list should be an event, so append it into the events list
         for index in range(len(more_parsed)):
-            if more_parsed[index].startswith("January") or more_parsed[index].startswith("February") or more_parsed[index].startswith("March")\
-                    or more_parsed[index].startswith("April") or more_parsed[index].startswith("May") or more_parsed[index].startswith("June")\
-                    or more_parsed[index].startswith("July") or more_parsed[index].startswith("August") or more_parsed[index].startswith("September")\
-                    or more_parsed[index].startswith("October") or more_parsed[index].startswith("November") or more_parsed[index].startswith("December"):
+            if more_parsed[index].startswith("january") or more_parsed[index].startswith("february") or more_parsed[index].startswith("march")\
+                    or more_parsed[index].startswith("april") or more_parsed[index].startswith("may") or more_parsed[index].startswith("june")\
+                    or more_parsed[index].startswith("july") or more_parsed[index].startswith("august") or more_parsed[index].startswith("september")\
+                    or more_parsed[index].startswith("october") or more_parsed[index].startswith("november") or more_parsed[index].startswith("december"):
                 dates.append(more_parsed[index])
                 events.append(more_parsed[index+1])
+
+        dates_with_year = self._combine_dates_with_year(dates) # Calls upon the dates_with_year helper method
+
+        date_events_dict = self._combine_date_events(dates_with_year, events)
+
+        # Create the JSON file
+        with open(self.filename_json, 'w') as f:
+            json.dump(date_events_dict, f, indent=4)
+
+    def _clean_up_text(self, data):
+        """Cleans up the text more, gets rid of backslashes and other html encodings"""
+        more_parsed = list()
+        for element in data:
+            data_parsed = element.strip()
+            more_refined = data_parsed.replace("\\", "")
+            even_more_refined = more_refined.replace("n'b'", "")
+            clean = even_more_refined.strip()
+            clean = clean.lower()
+            more_parsed.append(clean)       # Appended to an even more refined list
+
+        return more_parsed
+
+    def _combine_dates_with_year(self, dates):
+        """Helper method that will combine the dates with the year"""
+        # Create two lists, one for dates and the other for events
 
         dates_with_year = list()  # Creates a new list that will have the dates with year
         year = 2019     # The year we start at, 2019 needs to be fixed where it allows the program to be reusable, but for now its hardcoded in
         is_new_year = False
         # Appends the proper dates to each month
         for date in dates:
-            if date.startswith("January") and is_new_year is False:
+            if date.startswith("january") and is_new_year is False:
                 year += 1
                 is_new_year = True
-            elif date.startswith("January") and is_new_year is True:
+            elif date.startswith("january") and is_new_year is True:
                 pass
             else:
                 is_new_year = False
             dates_with_year.append(f'{date} {year}')
 
+        return dates_with_year
+
+    def _combine_date_events(self, dates_with_year, events):
+        """Helper method that creates a dictionary with each event corresponding to correct date"""
         date_events_dict = dict()
+
+        semesters = ['fall 2019', 'spring 2020']
+        count = 0
         # Creates a dictionary, each date is a key, and each event is a value corresponding to that key
-
         for index in range(len(dates_with_year)):
-            date_events_dict[events[index]] = dates_with_year[index]
+            key = events[index].replace("-", "for")
+            if key == 'finals begin' or key == 'finals end':  # Replaces the - with for
+                if count < 2:
+                    updated_key = key + " for " + semesters[0]
+                    count += 1
+                else:
+                    updated_key = key + " for " + semesters[1]
+                    count += 1
+                date_events_dict[updated_key] = dates_with_year[index]
+            elif key.find(" i ") is not -1 or key.find(" ii ") is not -1:  # Replaces the ii with first term or second term
+                if key.find(" i ") is not -1:
+                    updated_key = key.replace(" i ", " first term ")
+                elif key.find(" ii ") is not -1:
+                    updated_key = key.replace(" ii ", " second term ")
+                date_events_dict[updated_key] = dates_with_year[index]
+            elif key.find("/") is not -1:
+                updated_key = key.replace("/", " or ")
+                date_events_dict[updated_key] = dates_with_year[index]
+            elif key.find("for no classes") is not -1:
+                updated_key = key.replace("for no classes", "")
+                updated_key = updated_key.strip()
+                date_events_dict[updated_key] = dates_with_year[index]
+            else:
+                date_events_dict[key] = dates_with_year[index]
 
-        for key, value in date_events_dict.items():
-            if key.count("-") > 0 or key.count(" - ") > 0:
-                new_key = key.replace("-", "for")
-                new_key = new_key.replace(" - ", "for")
-                date_events_dict[new_key] = date_events_dict.pop(key)
+        return date_events_dict
 
-        for key, value in date_events_dict.items():
-            new_key = key.lower()
-            date_events_dict[new_key] = date_events_dict.pop(key)
 
-        with open(self.filename_json, 'w') as f:
-            json.dump(date_events_dict, f, indent=4)
+
+get_json = AdelphiInfo()
